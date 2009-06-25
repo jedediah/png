e468281381c98993094d6c656cbd7540e095483d
Jedediah Smith <jed@winky.(none)>
2009-06-25 02:21:34 -0400

    handle multiple IDAT chunks, fix single char bugs in Ruby 1.9, add .gitignore, debug mode

--- lib/png/reader.rb
+++ lib/png/reader.rb
@@ -4,6 +4,13 @@ require 'png'
 require 'enumerator'
 
 class PNG
+  class << self
+    attr_accessor :debug
+    def dmsg *a
+      STDERR.puts *a if @debug
+    end
+  end
+  
   def self.load_file path, metadata_only = false
     file = File.open(path, 'rb') { |f| f.read }
     self.load file, metadata_only
@@ -14,23 +21,31 @@ class PNG
     signature = png.slice! 0, 8
     raise ArgumentError, 'Invalid PNG signature' unless signature == SIGNATURE
     expected = ['IHDR','IDAT','IEND']
+    idat = ''
+    bit_depth = color_type = width = height = canvas = nil
 
-    until expected.empty?
+    while expected
       type, chunk = read_chunk png
       if expected[0] == type
         expected.shift
       elsif expected.include? type
         raise "unexpected chunk #{type}"
       end
+
+      if idat != '' && type != 'IDAT'
+        dmsg "decoding IDAT size=#{idat.size}"
+        read_IDAT idat, bit_depth, color_type, canvas
+      end
       
       case type
       when 'IHDR'
         bit_depth, color_type, width, height = read_IHDR chunk, metadata_only
+        dmsg "decoded IHDR width=#{width} height=#{height} bit_depth=#{bit_depth} color_type=#{color_type}"
         return [width, height, bit_depth] if metadata_only
         canvas = PNG::Canvas.new width, height
         expected = ['IDAT']
       when 'IDAT'
-        read_IDAT chunk, bit_depth, color_type, canvas
+        idat << chunk
         expected = ['IEND']
       when 'IEND'
         expected = nil
@@ -44,7 +59,7 @@ class PNG
     size, type = png.slice!(0, 8).unpack 'Na4'
     data, crc = png.slice!(0, size + 4).unpack "a#{size}N"
 
-    puts "chunk #{type} size=#{size} data.size=#{data.size}"
+    dmsg "chunk #{type} size=#{size} data.size=#{data.size}"
 
     check_crc type, data, crc
 
@@ -88,20 +103,20 @@ class PNG
       when NONE then
       when SUB then
         row_data.each_with_index do |byte, index|
-          left = index < pixel_size ? 0 : row_data[index - pixel_size]
+          left = index < pixel_size ? 0 : row_data[index - pixel_size].ord
           row_data[index] = (byte + left) % 256
         end
       when UP then
         row_data.each_with_index do |byte, index|
           col = index / pixel_size
-          upper = row == 0 ? 0 : canvas[col, row + 1].values[index % pixel_size]
+          upper = row == 0 ? 0 : canvas[col, row + 1].values[index % pixel_size].ord
           row_data[index] = (upper + byte) % 256
         end
       when AVG then
         row_data.each_with_index do |byte, index|
           col = index / pixel_size
-          upper = row == 0 ? 0 : canvas[col, row + 1].values[index % pixel_size]
-          left = index < pixel_size ? 0 : row_data[index - pixel_size]
+          upper = row == 0 ? 0 : canvas[col, row + 1].values[index % pixel_size].ord
+          left = index < pixel_size ? 0 : row_data[index - pixel_size].ord
 
           row_data[index] = (byte + ((left + upper)/2).floor) % 256
         end
@@ -110,13 +125,13 @@ class PNG
         row_data.each_with_index do |byte, index|
           col = index / pixel_size
 
-          left = index < pixel_size ? 0 : row_data[index - pixel_size]
+          left = index < pixel_size ? 0 : row_data[index - pixel_size].ord
           if row == height then
             upper = upper_left = 0
           else
-            upper = canvas[col, row + 1].values[index % pixel_size]
+            upper = canvas[col, row + 1].values[index % pixel_size].ord
             upper_left = col == 0 ? 0 :
-              canvas[col - 1, row + 1].values[index % pixel_size]
+              canvas[col - 1, row + 1].values[index % pixel_size].ord
           end
 
           paeth = paeth left, upper, upper_left
